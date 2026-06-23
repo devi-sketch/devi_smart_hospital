a9831e77d0c8a7213d68b1caae3f76cb359abda7
devi-sketch <devi@kodingnext.com>
2026-06-23 12:08:30 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -93,7 +93,7 @@ with st.form("triage_form"):
     with col_cc:
         chief_complaint = st.selectbox("Chief complaint", options=list(cc_map.keys()))
     with col_dur:
-        duration = st.selectbox("Duration", options=list(dur_map.keys()))
+        duration = st.selectbox("Duration", options=list(dur_map.keys()) , index=1)
 
     st.markdown("<br>", unsafe_allow_html=True)
 
